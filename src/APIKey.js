ab674dd5e2fbbafc5d1494b3f6550e4f2de39530
Vanshita24 <khilranivanshita@gmail.com>
2022-11-29 12:43:20 +0530

    $

---
+++ src/APIKey.js
@@ -0,0 +1,3 @@
+module.exports = {
+  APIKey: 'a7bcefba'
+};
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-dark mb-5">
+        <div className="container">
+          <div className="navbar-header">
+            <Link className="navbar-brand text-white text-lg brand-text" to="/">
+              Movie Search
+            </Link>
+          </div>
+          
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
